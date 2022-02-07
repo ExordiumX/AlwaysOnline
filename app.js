2cbc3c19e63a801caa921441cdac2f64434f69a5
Hacker-1202 <98639722+Hacker-1202@users.noreply.github.com>
2022-02-07 18:48:32 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const Eris = require("eris");
 const keepAlive = require("./server");
 keepAlive();
 
-const bot = new Eris(process.env.TOKEN);
+const bot = new Eris(process.env.OTQwMjg0NzUzMzAxNjA2NTEw.YgFK3g.syE2DYQCG05aH-NUWyLY-Csh98k);
 
 bot.connect();
